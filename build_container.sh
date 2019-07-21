9674190546c9828f30f430bfc1f47776e47f102f
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2019-07-21 17:15:11 +0200

    Make container image configurable

--- build_container.sh
+++ build_container.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
-container_image=docker://docker.io/library/python:3.7.4-alpine
+: ${PYTHON_VERSION:=3.7.4}
+: ${CONTAINER_NAME:=slack-wifi-updater}
+CONTAINER_IMAGE=docker://docker.io/library/python:${PYTHON_VERSION}-alpine
 
 function cleanup() {
     [[ -n "${container+x}" ]] && buildah rm $container
 }
 
+trap 'exit 1' INT HUP QUIT TERM ALRM USR1
 trap cleanup 0
 
-container_name=${1:-slack-wifi-updater}
-container=$(buildah from $container_image)
+container=$(buildah from $CONTAINER_IMAGE)
 
-buildah run $container -- sh -c "addgroup -S slack -g 1000 && adduser -S slack -G slack -u 1000"
+: ${USER_UID:=1000}
+buildah run $container -- sh -c "addgroup -S slack -g $USER_UID && adduser -S slack -G slack -u $USER_UID"
 buildah run $container -- apk --no-cache add wireless-tools
 buildah run $container -- pip3 install requests
 buildah copy $container ./check_wifi.py /usr/bin/check_wifi
 
 buildah config --user=slack --cmd=/usr/bin/check_wifi $container
-buildah commit $container $container_name
+buildah commit $container $CONTAINER_NAME
