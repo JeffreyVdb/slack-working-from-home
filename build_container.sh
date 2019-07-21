4a825c86dfc7735cc8504ef3f4704b2ec5038529
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2019-07-21 15:47:02 +0200

    Use python container

--- build_container.sh
+++ build_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-container_image=docker://docker.io/library/alpine:3.10
+container_image=docker://docker.io/library/python:3.7.4-alpine
 
 function cleanup() {
     [[ -n "${container+x}" ]] && buildah rm $container
@@ -9,11 +9,11 @@ function cleanup() {
 
 trap cleanup 0
 
-container_name=${1:-wireless-tools}
+container_name=${1:-slack-wifi-updater}
 container=$(buildah from $container_image)
 
 buildah run $container -- sh -c "addgroup -S slack -g 1000 && adduser -S slack -G slack -u 1000"
-buildah run $container -- apk --no-cache add python3 wireless-tools
+buildah run $container -- apk --no-cache add wireless-tools
 buildah run $container -- pip3 install requests
 buildah copy $container ./check_wifi.py /usr/bin/check_wifi
 
